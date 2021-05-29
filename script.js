10f3311852078a3f4b82996a313e2cb24546ea0e
Radek Rozycki <rroz1234@o2.pl>
2021-05-29 20:10:47 +0200

    Querying neighbours

--- script.js
+++ script.js
@@ -2,11 +2,26 @@
 
 const imageContainer = document.querySelector('.images');
 const countryInfo = document.getElementById('country_info');
+const button = document.getElementById('next');
+const img = document.querySelector('.country_img');
+
+const countriesDisplayed = [];
+const countriesToDisplay = [];
 
 const displayCountry = (data) => {
-    const html = `<img class="country__img" src="${data.flag}" />`
-    imageContainer.insertAdjacentHTML('beforeend', html);
+    //const html = `<img class="country__img" src="${data.flag}" />`;
+    //imageContainer.html = '';
+    //imageContainer.insertAdjacentHTML('beforeend', html);
+    img.src = data.flag;
     countryInfo.textContent = `${data.name}, ${data.capital}`
+}
+
+const retrieveConuntryByCode = async (code) => {
+    countriesDisplayed.push(code)
+    const response = await fetch(`https://restcountries.eu/rest/v2/alpha/${code}`)
+    const data = await response.json();
+    data.borders.filter(c=>countriesDisplayed.indexOf(c) === -1).forEach(c=>countriesToDisplay.push(retrieveConuntryByCode(c)));
+    return data;
 }
 
 const retrieveConuntry = (country) => {
@@ -21,4 +36,10 @@ const retrieveConuntry = (country) => {
     });
 }
 
-retrieveConuntry('Algeria')
+button.addEventListener('click', (e)=>{
+  e.preventDefault();
+  const nextCountryPromise = countriesToDisplay.shift().then(d=>displayCountry(d))
+})
+console.log('HERE')
+retrieveConuntryByCode('DZA')
+console.log('HERE')
