256e5e12437c1b0defd6bdf64ed1301dae2d97be
Radek Rozycki <rroz1234@o2.pl>
2021-05-29 14:46:50 +0200

    Retriving country from API

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-alert('Hello');
+'use strict';
 
+const imageContainer = document.querySelector('.images');
+const countryInfo = document.getElementById('country_info');
+
+const displayCountry = (data) => {
+    const html = `<img class="country__img" src="${data.flag}" />`
+    imageContainer.insertAdjacentHTML('beforeend', html);
+    countryInfo.textContent = `${data.name}, ${data.capital}`
+}
+
+const retrieveConuntry = (country) => {
+    fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      displayCountry(data[0]);
+    });
+}
+
+retrieveConuntry('Algeria')
