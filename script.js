412b4561a8de921d652d3dcbfbc5ba51c6f34016
Radek Rozycki <rroz1234@o2.pl>
2021-05-30 17:20:21 +0200

    Adding country data information

--- script.js
+++ script.js
@@ -2,25 +2,36 @@
 
 const imageContainer = document.querySelector('.images');
 const countryInfo = document.getElementById('country_info');
-const button = document.getElementById('next');
+const buttonNext = document.getElementById('next');
+const buttonPrevious = document.getElementById('previous');
 const img = document.querySelector('.country_img');
+let index = 0;
 
-const countriesDisplayed = [];
-const countriesToDisplay = [];
+const countriesStored = [];
+const countries = [];
 
 const displayCountry = (data) => {
-    //const html = `<img class="country__img" src="${data.flag}" />`;
-    //imageContainer.html = '';
-    //imageContainer.insertAdjacentHTML('beforeend', html);
     img.src = data.flag;
-    countryInfo.textContent = `${data.name}, ${data.capital}`
+
+    const html = `
+    <div class="country_data">
+      <h3 class="country_name">${data.name}</h3>
+      <h4 class="country_region">${data.subregion}</h4>
+      <p class="country_row"><i class="fas fa-city"></i>${data.capital}</p>
+      <p class="country_row"><i class="fas fa-male"></i>${(+data.population / 1000000).toFixed(2)}</p>
+      <p class="country_row"><i class="fab fa-speakap"></i>${data.languages.map(d=>d.name).join(", ")}</p>
+      <p class="country_row"><i class="fas fa-wallet"></i>${data.currencies.map(d=>`${d.name} (${d.symbol})`).join(", ")}</p>
+    </div>
+  `;
+    countryInfo.innerHTML = '';
+    countryInfo.insertAdjacentHTML('beforeend', html);
 }
 
 const retrieveConuntryByCode = async (code) => {
-    countriesDisplayed.push(code)
+    countriesStored.push(code)
     const response = await fetch(`https://restcountries.eu/rest/v2/alpha/${code}`)
     const data = await response.json();
-    data.borders.filter(c=>countriesDisplayed.indexOf(c) === -1).forEach(c=>countriesToDisplay.push(retrieveConuntryByCode(c)));
+    data.borders.filter(c=>!countriesStored.includes(c)).forEach(c=>countries.push(retrieveConuntryByCode(c)));
     return data;
 }
 
@@ -32,14 +43,29 @@ const retrieveConuntry = (country) => {
     })
     .then(data => {
       console.log(data);
+      data[0].borders.filter(c=>!countriesStored.includes(c)).forEach(c=>countries.push(retrieveConuntryByCode(c)));
       displayCountry(data[0]);
     });
 }
 
-button.addEventListener('click', (e)=>{
+buttonNext.addEventListener('click', (e)=>{
   e.preventDefault();
-  const nextCountryPromise = countriesToDisplay.shift().then(d=>displayCountry(d))
+  console.log(countriesStored, index)
+  if(countriesStored.length > index + 1) {
+    index++;
+    console.log(countriesStored, index)
+    const nextCountryPromise = countries[index];
+    nextCountryPromise.then(d=>displayCountry(d));
+  }
 })
-console.log('HERE')
-retrieveConuntryByCode('DZA')
-console.log('HERE')
+
+buttonPrevious.addEventListener('click', (e)=>{
+  e.preventDefault();
+  if(index > 0) {
+    const nextCountryPromise = countries[--index];
+    nextCountryPromise.then(d=>displayCountry(d));
+  }
+})
+
+retrieveConuntry('China')
+
