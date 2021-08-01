870f3fde30e1882d71f0c03bb9ee9045d00ab9a2
Radek Rozycki <rroz1234@o2.pl>
2021-08-01 21:37:08 +0200

    Refactoring code for class usage

--- script.js
+++ script.js
@@ -5,43 +5,111 @@ const imageContainer = document.querySelector('.images');
 const countryInfo = document.getElementById('country_info');
 const buttonNext = document.getElementById('next');
 const buttonPrevious = document.getElementById('previous');
+const buttonSearch = document.getElementById('search');
+const btnCloseModal = document.querySelector('.close-modal');
 const img = document.querySelector('.country_img');
-let index = 0;
-var map = L.map('map');
-map.createPane('labels');
-map.getPane('labels').style.zIndex = 650;
-map.getPane('labels').style.pointerEvents = 'none';
 
-var positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
-        attribution: '©OpenStreetMap, ©CartoDB'
-}).addTo(map);
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
 
-var positronLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
-        attribution: '©OpenStreetMap, ©CartoDB',
-        pane: 'labels'
-}).addTo(map);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-// var geojson = L.geoJson(euCountries).addTo(map);
-// geojson.eachLayer(function (layer) {
-//   layer.bindPopup(layer.feature.properties.name);
-// });
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
-// map.fitBounds(geojson.getBounds());
 
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
 
-// L.marker([51.5, -0.09]).addTo(map)
-//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-//     .openPopup();
 
-const countriesStored = [];
-const countries = [];
-let marker;
-let geojson;
 
-const displayCountry = (data) => {
+
+
+class Application {
+  map;
+  retrievedcountriesData = [];
+  retrievedCountriesCodes = [];
+  index = 0;
+  countriesStored = [];
+  countries = [];
+  markers = [];
+  geojson;
+
+  constructor() {
+    this._init();
+    
+  }
+
+  _init() {
+    this._initMap();
+    this._initButtons();
+  }
+
+  _initMap(){
+    this.map = L.map('map');
+    this.map.createPane('labels');
+    this.map.getPane('labels').style.zIndex = 650;
+    this.map.getPane('labels').style.pointerEvents = 'none';
+
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+            attribution: '©OpenStreetMap, ©CartoDB'
+    }).addTo(this.map);
+
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
+            attribution: '©OpenStreetMap, ©CartoDB',
+            pane: 'labels'
+    }).addTo(this.map);
+
+    this.map.on('click',(w)=>console.log(w))
+  }
+
+  _initButtons() {
+    buttonNext.addEventListener('click', (e)=>{
+      e.preventDefault();
+      if(this.retrievedCountriesCodes.length > this.index + 1) {
+        this.index++;
+        const nextCountryPromise = this.retrievedcountriesData[this.index];
+        nextCountryPromise.then(d=>this._displayCountry(d));
+      }
+    })
+    
+    buttonPrevious.addEventListener('click', (e)=>{
+      e.preventDefault();
+      if(this.index > 0) {
+        const nextCountryPromise = this.retrievedcountriesData[--this.index];
+        nextCountryPromise.then(d=>this._displayCountry(d));
+      }
+    })
+
+    buttonSearch.addEventListener('click', openModal);
+    btnCloseModal.addEventListener('click', closeModal);
+  }
+
+  async retrieveConuntryByCode(code){
+    this.retrievedCountriesCodes.push(code);
+    const response = await fetch(`https://restcountries.eu/rest/v2/alpha/${code}`);
+    const data = await response.json();
+    data.borders.filter(c=>!this.retrievedCountriesCodes.includes(c)).forEach(c=>this.retrievedcountriesData.push(this.retrieveConuntryByCode(c)));
+    return data;
+}
+
+  retrieveConuntryByName(country){
+      fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        data[0].borders.filter(c=>!this.retrievedCountriesCodes.includes(c)).forEach(c=>this.retrievedcountriesData.push(this.retrieveConuntryByCode(c)));
+        this._displayCountry(data[0]);
+      });
+  }
+
+  _displayCountry(data){
     const html = `
     <div class="country_data">
       <img class="country_img" src="${data.flag}" />
@@ -57,12 +125,28 @@ const displayCountry = (data) => {
     countryInfo.insertAdjacentHTML('beforeend', html);
 
     const zoom = data.area > 10000000 ? 1 : (data.area > 7000000 ? 2 : (data.area > 3000000 ? 3 : (data.area > 1000000 ? 4 : (data.area > 300000 ? 5 : 6))))
-    map.setView(data.latlng, zoom);
-    if(marker){marker.remove();}
-    marker = L.marker(data.latlng);
-    marker.addTo(map)
-    .bindPopup(data.name)
-    .openPopup();
+    this.map.setView(data.latlng, zoom);
+    this.markers.forEach(m=>m.remove());
+  
+    const mainCities = cities[data.alpha3Code];
+    if(mainCities) {
+      mainCities.forEach(c=>{
+        const marker = L.marker(c.latlng);
+        marker.addTo(this.map)
+        .bindPopup(
+          L.popup({
+            autoClose: false,
+            closeOnClick: false,
+            //className: `${workout.type}-popup`,
+          })
+        )
+        .setPopupContent(
+          c.city
+        )
+        .openPopup();
+        this.markers.push(marker);
+      })
+    }
 
     var states = borderPoints[data.alpha3Code].map(arr=>{return {
       "type": "Feature",
@@ -73,58 +157,19 @@ const displayCountry = (data) => {
       }
   }});
   
-  if(geojson){geojson.remove();}
-  geojson = L.geoJSON(states, {
-      style: function(feature) {
-          switch (feature.properties.party) {
-              case 'Republican': return {color: "#ff0000"};
-              case 'Democrat':   return {color: "#0000ff"};
+    if(this.geojson){this.geojson.remove();}
+      this.geojson = L.geoJSON(states, {
+          style: function(feature) {
+              switch (feature.properties.party) {
+                  case 'Republican': return {color: "#ff0000"};
+                  case 'Democrat':   return {color: "#0000ff"};
+              }
           }
-      }
-  });
+      });
 
-  geojson.addTo(map);
-}
-
-const retrieveConuntryByCode = async (code) => {
-    countriesStored.push(code)
-    const response = await fetch(`https://restcountries.eu/rest/v2/alpha/${code}`)
-    const data = await response.json();
-    data.borders.filter(c=>!countriesStored.includes(c)).forEach(c=>countries.push(retrieveConuntryByCode(c)));
-    return data;
-}
-
-const retrieveConuntry = (country) => {
-    fetch(`https://restcountries.eu/rest/v2/name/${country}`)
-    .then(response => {
-      console.log(response);
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-      data[0].borders.filter(c=>!countriesStored.includes(c)).forEach(c=>countries.push(retrieveConuntryByCode(c)));
-      displayCountry(data[0]);
-    });
-}
-
-buttonNext.addEventListener('click', (e)=>{
-  e.preventDefault();
-  console.log(countriesStored, index)
-  if(countriesStored.length > index + 1) {
-    index++;
-    console.log(countriesStored, index)
-    const nextCountryPromise = countries[index];
-    nextCountryPromise.then(d=>displayCountry(d));
+      this.geojson.addTo(this.map);
   }
-})
+}
 
-buttonPrevious.addEventListener('click', (e)=>{
-  e.preventDefault();
-  if(index > 0) {
-    const nextCountryPromise = countries[--index];
-    nextCountryPromise.then(d=>displayCountry(d));
-  }
-})
-
-retrieveConuntry('Canada')
-
+const app = new Application();
+app.retrieveConuntryByName("Poland");
